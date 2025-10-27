134bbecb83b20ea35267f2aea620a94918937437
0re0-Speedwagon <Mw3Rodgers@gmail.com>
2025-10-27 18:36:03 -0400

    Reset button added

---
+++ main.js
@@ -0,0 +1,26 @@
+const A_Legion = document.getElementById("A_Legion");
+const D_Legion = document.getElementById("D_Legion");
+const attacker = document.getElementById("attacker");
+const defender = document.getElementById("defender");
+const reset = document.getElementById("reset");
+const generate = document.getElementById("generate");
+
+reset.addEventListener("click", function () {
+  console.log("reset!");
+  A_Legion.value = "start";
+  D_Legion.value = "start";
+  attacker.value = "start";
+  defender.value = "start";
+});
+A_Legion.addEventListener("mouseleave", function () {
+  console.log(A_Legion.value);
+});
+attacker.addEventListener("mouseleave", function () {
+  console.log(attacker.value);
+});
+D_Legion.addEventListener("mouseleave", function () {
+  console.log(D_Legion.value);
+});
+defender.addEventListener("mouseleave", function () {
+  console.log(defender.value);
+});
