3f376142394afc804118ef67dad3b1bb2ba2273d
0re0-Speedwagon <Mw3Rodgers@gmail.com>
2026-03-27 13:22:31 -0400

    itteration count changed

--- backend/server.py
+++ backend/server.py
@@ -223,7 +223,7 @@ async def calculate(request: CalcRequest):
     keywords = request.kwords       #keywords
     calc = []                       #array of calculations
 
-    for i in range(40):
+    for i in range(60):
         calc.append(Calculations())
         modelattacks = ainput.attacks
         if ainput.amodels * ainput.abulky < dinput.dmodels * dinput.dbulky:
